25dd70e668ecfa89d81878c8241998af5ba15a78
Слава <slavaspringerr@gmail.com>
2024-11-30 19:28:47 +0400

    Первая реализация ТЗ 7ого спринта.

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,39 @@ import manager.*;
 import task.Epic;
 import task.Status;
 import task.Subtask;
+import task.Task;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        TaskManager taskManager = Managers.getDefault();
-        Epic epic1 = new Epic("Epic-1","Epic-1",Status.NEW);
-        Epic epic2 = new Epic("Epic-2","Epic-2",Status.NEW);
 
-        Subtask subtask1 = new Subtask("Subtask-1","Subtask-1 for Epic-1",Status.NEW,epic1);
-        Subtask subtask2 = new Subtask("Subtask-2","Subtask-2 for Epic-1",Status.NEW,epic1);
+        File file = new File("src/loadFile.csv");
+        System.out.println(FileBackedTaskManager.loadFromFile(file).getHistory());
+        FileBackedTaskManager taskManager = new FileBackedTaskManager(Managers.getDefaultHistory());
+        Epic epic1 = new Epic("Epic-1","Epic-1",Status.IN_PROGRESS);
+        Epic epic2 = new Epic("Epic-2","Epic-2",Status.NEW);
+        Task task1 = new Task("Task-1","description for task-1",Status.NEW);
+
+
+        Subtask subtask1 = new Subtask("Subtask-1","Subtask-1 for Epic-1",Status.DONE,epic1);
+        Subtask subtask2 = new Subtask("Subtask-2","Subtask-2 for Epic-1",Status.IN_PROGRESS,epic1);
         Subtask subtask3 = new Subtask("Subtask-3","Subtask-3 for Epic-1",Status.NEW,epic1);
+        taskManager.createTask(task1);
         taskManager.createEpic(epic1);
         taskManager.createEpic(epic2);
+
         taskManager.createSubtask(subtask1);
         taskManager.createSubtask(subtask2);
         taskManager.createSubtask(subtask3);
 
-        taskManager.getEpic(1);
+
+
+      /*  taskManager.getEpic(1);
         System.out.println(taskManager.getHistory());
         taskManager.getSubtask(3);
         System.out.println(taskManager.getHistory());
@@ -37,7 +51,7 @@ public class Main {
         taskManager.deleteSubtask(4);
         System.out.println(taskManager.getHistory());
         taskManager.deleteEpic(1);
-        System.out.println(taskManager.getHistory());
+        System.out.println(taskManager.getHistory());*/
 
     }
 
---
+++ src/manager/FileBackedTaskManager.java
@@ -0,0 +1,191 @@
+package manager;
+
+import task.Epic;
+import task.Status;
+import task.Subtask;
+import task.Task;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager{
+    private  String pathToFile = "src/file.csv";
+      private Path file;
+
+
+    public Path getFile() {
+        return file;
+    }
+
+    public String getPathToFile() {
+        return pathToFile;
+    }
+
+    public FileBackedTaskManager(HistoryManager defaultHistory, Path file) {
+        super(defaultHistory);
+        this.pathToFile = file.toAbsolutePath().toString();
+        this.file = file;
+    }
+
+    public FileBackedTaskManager(HistoryManager defaultHistory) {
+        super(defaultHistory);
+        this.file = Paths.get(pathToFile);
+
+    }
+
+
+
+
+    @Override
+    public Task createTask(Task task) {
+        super.createTask(task);
+
+        save();
+        return task;
+    }
+
+    @Override
+    public Task createEpic(Epic epic) {
+        super.createEpic(epic);
+
+        save();
+        return epic;
+
+
+    }
+
+    @Override
+    public Task createSubtask(Subtask subtask) {
+        super.createSubtask(subtask);
+
+        save();
+        return subtask;
+
+    }
+
+
+    public String toString(Task task) {
+        return task.getId()+","+task.getType() + "," + task.getName() + "," + task.getDescription() + "," + task.getStatus();
+    }
+
+   /* public void save(Task task){
+
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(pathToFile,true))){
+
+            writer.write(toString(task)+"\n");
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }*/
+    public void save(){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathToFile))){
+
+            bw.write("id,type,name,status,description,epic\n");
+            for (Task task : getTasks()){
+                bw.write(toString(task)+"\n");
+            }
+            for (Task epic : getEpics()){
+                bw.write(toString(epic)+"\n");
+            }
+            for (Subtask subtask : getSubtasks()){
+                bw.write(toString(subtask)+","+subtask.getEpic().getId()+"\n");
+            }
+
+
+        } catch (IOException e) {
+            throw new ManagerSaveException(e);
+        }
+
+    }
+
+    public Task fromString(String value){
+        String[] mass = value.split(",");
+
+        if (mass[1].equals("TASK")){
+            Status status;
+            if (mass[4].equals("NEW")){
+                 status = Status.NEW;
+            }
+            else if (mass[4].equals("DONE")){
+                 status = Status.DONE;
+            }
+            else { status = Status.IN_PROGRESS;}
+            Task task = new Task(Integer.parseInt(mass[0]),mass[2],mass[3], status );
+            task.setType(TaskType.TASK);
+            createTask(task);
+            return task;
+        }
+        else if (mass[1].equals("EPIC")){
+            Status status;
+            if (mass[4].equals("NEW")){
+                status = Status.NEW;
+            }
+            else if (mass[4].equals("DONE")){
+                status = Status.DONE;
+            }
+            else { status = Status.IN_PROGRESS;}
+            Epic epic = new Epic(Integer.parseInt(mass[0]),mass[2],mass[3], status );
+            epic.setType(TaskType.EPIC);
+            createEpic(epic);
+            return epic;
+        }
+        else {
+            Status status;
+            if (mass[4].equals("NEW")){
+                status = Status.NEW;
+            }
+            else if (mass[4].equals("DONE")){
+                status = Status.DONE;
+            }
+            else { status = Status.IN_PROGRESS;}
+            Subtask subtask = new Subtask(Integer.parseInt(mass[0]),mass[2],mass[3], status,getEpic(Integer.parseInt(mass[5])) );
+            subtask.setType(TaskType.SUBTASK);
+            createSubtask(subtask);
+            return subtask;
+        }
+
+
+    }
+    public static FileBackedTaskManager loadFromFile(File file){
+        FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(Managers.getDefaultHistory());
+        try {
+            List<String> lines = Files.readAllLines(file.toPath());
+            for (String string : lines){
+                if (!string.equals(lines.get(0))){
+                    fileBackedTaskManager.fromString(string);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return fileBackedTaskManager;
+    }
+    public class ManagerSaveException extends RuntimeException{
+
+
+        public ManagerSaveException(final Throwable cause) {
+            super(cause);
+        }
+
+
+
+
+    }
+
+
+}
